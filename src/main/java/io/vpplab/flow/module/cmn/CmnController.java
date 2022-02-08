e2726bfda2b6ae5925538e7527aca6e18619edf1
angrypark <angrypark@tivansoft.com>
2022-02-08 10:02:17 +0900

    메뉴샘플

--- src/main/java/io/vpplab/flow/module/cmn/CmnController.java
+++ src/main/java/io/vpplab/flow/module/cmn/CmnController.java
@@ -15,22 +15,12 @@ public class CmnController {
     private CmnService cmnService;
 
 
-/*
- frontend 파라미터 전달  예)
- var axios = require('axios');
- var FormData = require('form-data');
- var data = new FormData();
- data.append('로그인ID', 'donghae');*/
+
 
 
     @RequestMapping("/cmn/getLogin")
-    public HashMap getLogin(@RequestParam HashMap<String,String> paramMap) {
+    public Map<String, Object> getLogin(@RequestParam HashMap<String,String> paramMap) {
         return cmnService.getLogin(paramMap);
-    }
-
-    @RequestMapping("/cmn/getMenuMng")
-    public Map<String, Object> getMenuMng(@RequestParam HashMap<String,String> paramMap) {
-        return cmnService.getMenuMng(paramMap);
     }
 
 }
--- src/main/java/io/vpplab/flow/module/cmn/CmnService.java
+++ src/main/java/io/vpplab/flow/module/cmn/CmnService.java
@@ -19,55 +19,51 @@ public class CmnService {
     @Autowired
     private CmnDao cmnDao;
 
-    public HashMap getLogin(HashMap<String,String> paramMap) {
-       //  paramMap.put("로그인ID","donghae");
-        HashMap<String,String> hashMap  =  cmnDao.getLogin(paramMap);
-        if(hashMap == null){
-            hashMap = new HashMap<>();
-            hashMap.put("상태","01");
-        }else{
-            hashMap.put("상태","00");
-        }
-        return hashMap;
-    }
-
-
-    public Map<String, Object> getMenuMng(HashMap<String,String> paramMap) {
-        Map<String, Object> menuMap = new HashMap<>();
+    public Map<String, Object> getLogin(HashMap<String,String> paramMap) {
+        HashMap<String,String> loginMap  =  cmnDao.getLogin(paramMap);
+        Map<String, Object> multiMap = new HashMap<>();
         List<Map<String, Object>> menuTree = new ArrayList<Map<String, Object>>();
         List<HashMap> menuList  =  cmnDao.getMenuMng(paramMap);
 
         int cnt =0;
-        if(menuList.size() > 0){
-            for(int i = 0; i < menuList.size(); i++){
-                if(!"".equals(menuList.get(i).get("1차메뉴")) && menuList.get(i).get("1차메뉴") != null){
-                    Map<String, Object> dep1Map = new HashMap<>();
-                    dep1Map.put( "URL주소",menuList.get(i).get("URL주소"));
-                    dep1Map.put( "화면이름",menuList.get(i).get("화면이름"));
-                    dep1Map.put( "화면여부",menuList.get(i).get("화면여부"));
-                    dep1Map.put( "수정권한여부",menuList.get(i).get("수정권한여부"));
-                    menuTree.add(cnt,dep1Map);
-                    List<HashMap> sub  =  new ArrayList<>();
-                    for(int j = 0; j < menuList.size(); j++){
-                        if((menuList.get(i).get("1차메뉴")).equals(menuList.get(j).get("2차메뉴")) && ("".equals(menuList.get(j).get("1차메뉴")) || menuList.get(j).get("1차메뉴") == null)){
-                            HashMap dep2Map = new HashMap<String,Object>();
-                            dep2Map.put( "URL주소",menuList.get(j).get("URL주소"));
-                            dep2Map.put( "화면이름",menuList.get(j).get("화면이름"));
-                            dep2Map.put( "화면여부",menuList.get(j).get("화면여부"));
-                            dep2Map.put( "수정권한여부",menuList.get(j).get("수정권한여부"));
-                            sub.add(dep2Map);
+        if(loginMap != null){
+            if(menuList.size() > 0){
+                for(int i = 0; i < menuList.size(); i++){
+                    if(!"".equals(menuList.get(i).get("1차메뉴")) && menuList.get(i).get("1차메뉴") != null){
+                        Map<String, Object> dep1Map = new HashMap<>();
+                        dep1Map.put( "URL주소",menuList.get(i).get("URL주소"));
+                        dep1Map.put( "화면이름",menuList.get(i).get("화면이름"));
+                        dep1Map.put( "화면여부",menuList.get(i).get("화면여부"));
+                        dep1Map.put( "수정권한여부",menuList.get(i).get("수정권한여부"));
+                        menuTree.add(cnt,dep1Map);
+                        List<HashMap> sub  =  new ArrayList<>();
+                        for(int j = 0; j < menuList.size(); j++){
+                            if((menuList.get(i).get("1차메뉴")).equals(menuList.get(j).get("2차메뉴")) && ("".equals(menuList.get(j).get("1차메뉴")) || menuList.get(j).get("1차메뉴") == null)){
+                                HashMap dep2Map = new HashMap<String,Object>();
+                                dep2Map.put( "URL주소",menuList.get(j).get("URL주소"));
+                                dep2Map.put( "화면이름",menuList.get(j).get("화면이름"));
+                                dep2Map.put( "화면여부",menuList.get(j).get("화면여부"));
+                                dep2Map.put( "수정권한여부",menuList.get(j).get("수정권한여부"));
+                                sub.add(dep2Map);
+                            }
                         }
+                        menuTree.get(cnt).put("2차메뉴",sub);
+                        cnt++;
                     }
-                    menuTree.get(cnt).put("2차메뉴",sub);
-                    cnt++;
                 }
-            }
 
-            menuMap.put("1차메뉴",menuTree);
-            menuMap.put("상태","00");
+                multiMap.put("1차메뉴",menuTree);
+            }
+            multiMap.put("로그인정보",loginMap);
+            multiMap.put("조회여부","1");
+
         }else{
-            menuMap.put("상태","01");
+            multiMap.put("로그인정보",null);
+            multiMap.put("1차메뉴",null);
+            multiMap.put("조회여부","0");
         }
-        return menuMap;
+        return multiMap;
     }
+
+
 }
